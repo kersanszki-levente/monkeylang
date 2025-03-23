837dc01a5efb4e34f504bd9e3d37eb43788dc62b
kersanszki-levente <kersanszki.levente@gmail.com>
2025-03-23 19:20:26 +0100

    Added support for reading and evaluating files

--- repl/src/main.rs
+++ repl/src/main.rs
@@ -1,12 +1,14 @@
 #![allow(unused_imports)]
 
 use std::io::{
-    stdin,
-    BufRead,
-    BufReader,
-    Write,
+    stdin, BufRead, BufReader, Read, Write
 };
+use std::fmt::Debug;
+use std::fmt::Display;
+use std::fs::File;
+use std::path::Path;
 
+use clap::Parser as ClapParser;
 use termcolor::{
     Color,
     ColorChoice,
@@ -26,6 +28,22 @@ const PROMPT: &str=">>>";
 
 const HELP: &str=r#".help    Prints this message to the screen
 .exit    Exit the interpreter"#;
+
+struct RuntimeError(String);
+
+impl Debug for RuntimeError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl Display for RuntimeError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+type RuntimeResult<T>=Result<T, RuntimeError>;
 
 fn prompt(mut output: &mut StandardStream) -> Result<(), std::io::Error> {
     output.set_color(ColorSpec::new().set_fg(Some(Color::Rgb(255, 165, 0))))?;
@@ -94,7 +112,53 @@ fn start() -> Result<(), std::io::Error> {
     Ok(())
 }
 
+fn read_source_file(path: String) -> RuntimeResult<String> {
+    if !Path::new(&path).exists() {
+        return Err(RuntimeError(format!("{path} file does not exist")))
+    }
+    let mut buffer = String::new();
+    BufReader::new(File::open(&path).map_err(|_| RuntimeError(format!("Failed to read file {}", &path)))?)
+        .read_to_string(&mut buffer)
+        .map_err(|_| RuntimeError(format!("Failed to read file {}", &path)))?;
+    Ok(buffer)
+}
+
+fn evaluate_source_file(source_file_path: String) -> RuntimeResult<()> {
+    let buffer = read_source_file(source_file_path)?;
+    let lexer = Lexer::new(&buffer);
+    let mut parser = Parser::new(lexer);
+
+    let program = parser.parse();
+    if let Some(err) = parser.errors.first() {
+        return Err(RuntimeError(format!("Parsing error: {}", err)));
+    };
+
+    let env = Environment::new_shared(None);
+    match program.eval(&env) {
+        Ok(result) => {
+            match result {
+                Value::Null => {},
+                _ => return Err(RuntimeError(format!("{}", result))),
+            }
+        },
+        Err(err) => return Err(RuntimeError(format!("{:?}", err))),
+    };
+    Ok(())
+}
+
+#[derive(Debug, ClapParser)]
+struct Arguments {
+    file: Option<String>,
+}
+
 fn main() {
+    let args = Arguments::parse();
+    if let Some(source_file) = args.file {
+        if let Err(err) = evaluate_source_file(source_file) {
+            eprintln!("{err}");
+        };
+        std::process::exit(0);
+    }
     if let Err(err) = start() {
         eprintln!("{err}");
         std::process::exit(1);
